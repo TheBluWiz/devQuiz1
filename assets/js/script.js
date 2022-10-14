297fe30217b236e36ca595be0ab8cd4932a220a8
Jamey Wicklund <theBluWiz@icloud.com>
2022-10-14 10:08:04 -0700

    Sets starting state of UI

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
 var timeRemaining = 0
 
 var timeRemainingEl = document.querySelector("#time-remaining")
-timeRemainingEl.textContent = "Time Remaining: " + timeRemaining
+timeRemainingEl.textContent = timeRemaining
