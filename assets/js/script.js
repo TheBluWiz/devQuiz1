10ebb2ae21540c861131826c6f004db2bf073169
Jamey Wicklund <theBluWiz@icloud.com>
2022-10-14 15:01:29 -0700

    Adds example question object for testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,19 @@
-var timeRemaining = 0
+var timeRemaining = 0;
 
-var timeRemainingEl = document.querySelector("#time-remaining")
-timeRemainingEl.textContent = timeRemaining
+var timeRemainingEl = document.querySelector("#time-remaining");
+timeRemainingEl.textContent = timeRemaining;
+
+
+
+const questions = {
+  question1: {
+    question: "Commonly used data types DO NOT include",
+    answers: ["strings", "booleans", "alerts", "numbers"],
+    correctAnswer: "alerts"
+  },
+  question2: {
+    
+  }
+}
+
+console.log(questions)
